819ed591f89c5dd6aee9db79ab8c827ed06b60b2
Abhishek Patre <abhishek.patre@shutterfly.com>
2021-01-04 18:06:09 +0530

    Modified Code

--- src/test/java/StringOps.java
+++ src/test/java/StringOps.java
@@ -1,15 +1,17 @@
 public class StringOps {
     public static void main(String[] args) {
         String str = "My name is Abhi. Abhi works in Clairvoyant. Clairvoyant is in India";
+        findDuplicatesInString(str);
+    }
+
+    public static void findDuplicatesInString(String str){
         String arrStr[] = str.replace(".","").split(" ");
         for(int i = 0; i<arrStr.length-1; i++){
             for( int j = i+1; j<arrStr.length-1; j++){
-               if(arrStr[i].equals(arrStr[j])){
-                   System.out.println(arrStr[i]+" --> has a duplicate at array index: -->"+j);
-               }
+                if(arrStr[i].equals(arrStr[j])){
+                    System.out.println(arrStr[i]+" --> has a duplicate at array index: -->"+j);
+                }
             }
         }
     }
-
-
 }
